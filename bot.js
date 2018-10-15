d185b25fe8fd1486dbc8705f5c02f82b798cc7b2
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-15 16:45:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const prefix = '-';
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   client.user.setGame(`${client.user.username}`, "https://twitch.tv//9ivv")
