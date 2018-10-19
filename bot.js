8488ccefb2633b70bdc484fb28c0f9979579293d
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-19 19:22:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,22 @@ client.on('ready', () => {
     console.log('')
   });
 
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+        let room = m.guild.channels.find(a => a.name === 'united');
+            room.send(`**:no_entry: | ${m} Has been banned for: \`fake\`**`);
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
 
 
 client.on('message', message => {
